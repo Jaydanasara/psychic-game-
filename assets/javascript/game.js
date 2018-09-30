fb1b58f0b0971ec36c0f9b37331056409ca62376
jay <jaydanasara@yahoo.com>
2018-09-30 15:40:46 -0400

    bug version

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// var word_to_guess="test"
+// var good_letters=[];
+//  function print() {
+//      var print_word=" "
+//     document.getElementById("hangman").textContent=print_word;
+//     for (var i = 0; i< word_to_guess.length; i++){
+//         if (good_letters.indexOf (word_to_guess[i]) >-1) {
+//             print_word+=" " + word_to_guess[i] + " ";
+//      } else {
+//          print_word+=" _ "
+//      }
+
+//      }
+//     document.getElementById("hangman").textContent= print_word;
+// }
+// document.onkeyup =function (event) {
+
+//     if (word_to_guess.indexOf(event.key) >-1){
+//         good_letters.push (event.key);
+//  } 
+//         print();
+// }
---
+++ assets/javascript/game2.js
@@ -0,0 +1,67 @@
+// var words = ["nwa", "publicenemy", "llcoolj", "bigdaddykane"];
+// var wins = 0;
+// var losses = 0;
+// var guessesleft = 9
+
+// //The User's Guess
+// var yourGuess;
+// // var icl = [];
+
+// var cl = [];
+
+document.getElementById("hangman").textContent= "";
+var word =""
+// var wordchoice = words[Math.floor(Math.random() * words.length)];
+
+
+// function print() {
+//     var print_word =( "")
+
+//     document.getElementById("hangman").textContent ="";
+
+//     for (var j = 0; j < wordchoice.length; j++) {
+
+//         //Checking yourGuess with the word choice, i.e. the computer choice
+
+//         if (yourGuess.indexOf(wordchoice[j]) > -1) {
+//             print_word += "" + yourGuess[j] + "";
+//         } else {
+//             print_word += "_";
+//         }
+//     }console.log (print);
+// }
+
+// document.onkeyup = function(event){
+//     yourGuess = event.key;
+// }
+
+
+//loop through each leter in wordchoice
+// if letter is in cl, print letter 
+// else print _
+
+//var dash= indexOf(wordchoice)
+
+//check for picked letter
+
+//for (var j=0; j<wordchoice.length;j++)
+// if (wordchoice,indexOf(event.key)>-1){
+//     cl.push(event.key);
+// }else {
+//     icl.push(yourguess)
+// }
+
+// document.getElementById("hangman").textContent=yourguess 
+
+
+
+
+
+
+// var html="<p> Press any key to start playing! </p>"+
+// "<p>Wins:"+ wins + "</p>" +
+// "<p>Losses:" + losses + "</p>" +
+// "<p>guesses left:" + guessesleft + "</p>"+
+// "<p>your guesses:"+ yourguesses + "</p">";
+
+// document.querySelector ('#game').innerHTML = html;
